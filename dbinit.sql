022df3403b973e4c04ecc65329c8142ca89fdd7b
Daniel Araujo <codkrdaniel@gmail.com>
2020-01-14 14:13:59 -0300

    Novos arquivos no projeto

--- dbinit.sql
+++ dbinit.sql
@@ -8,7 +8,10 @@ CREATE TABLE IF NOT EXISTS `access` (
   `login` varchar(50) NOT NULL,
   `pass` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `access` (`id`, `login`, `pass`) VALUES
+    (1, 'admin', '$2y$10$O7G1a7TvErtyWnFxuXtGCOrhOx9fqU8ry9QsIzil5DnBzP5H01OhC');
 
 -- Copiando estrutura para tabela crud_login.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -16,5 +19,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(50) NOT NULL,
   `surname` varchar(50) NOT NULL,
   `caddate` date NOT NULL,
+  `status` int(1) DEFAULT 1,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
